1a8866cefa70cbf82c360e8bb800f1b0f47d68e7
shady <shadygamal845@gmail.com>
2024-02-12 14:29:45 +0200

    Models Added

---
+++ app/Models/Allergies_Info.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Allergies_Info extends Model
+{
+    use HasFactory;
+}
